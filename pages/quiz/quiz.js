2c8c32ae1f76a1887ea4bf9dce8f5d2afad43190
sara tagliaferro <saratagliaferro@outlook.com>
2024-06-06 03:01:08 +0000

    adição do javascript do quiz.html para trocar o tema

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,4 +1,4 @@
-import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
+import {verificarTema, trocarTema} from "../../helpers/tema-helper.js"
 
 const botaoTema = document.querySelector (".tema button")
 const body = document.querySelector("body")
@@ -6,5 +6,6 @@ const body = document.querySelector("body")
 botaoTema.addEventListener("click",() => {
    trocarTema(body, botaoTema)
 })
+
 
 verificarTema(body, botaoTema)
